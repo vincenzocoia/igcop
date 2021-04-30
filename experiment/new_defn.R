5bd1b55850f522a51d48abf33b9f2950133f8ab9
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-04-30 10:42:40 -0700

    try small k with new parameterization -- it works!

--- experiment/new_defn.R
+++ experiment/new_defn.R
@@ -9,6 +9,61 @@ psi_new_D <- function(x, k) {
   - (k - 1) * stats::pgamma(x, k) / x ^ 2
 }
 
+psi_new_inv_algo <- function(p, k, mxiter = 20, eps = 1.e-12, bd = 5){
+  if (length(p) != 1L) stop("Algorithm requires a single `p`.")
+  if (length(k) != 1L) stop("Algorithm requires a single `k`.")
+  if (p == 0) return(Inf)
+  if (p == 1) return(1)
+  x1 <- (k - 1) / p
+  x2 <- stats::qgamma(1 - p, k - 1, lower.tail = FALSE)
+  p1 <- psi_new(x1, k)
+  p2 <- psi_new(x2, k)
+  if (abs(p - p1) < abs(p - p2)) {
+    x <- x1
+    cat("inv")
+  } else {
+    x <- x2
+    cat("qgamma")
+  }
+  iter <- 0
+  diff <- 1
+  ## Begin Newton-Raphson algorithm
+  while (iter < mxiter & abs(diff) > eps) {
+    # g = x * psi(x) - x * p = 0
+    g <- (k - 1) * stats::pgamma(x, k) +
+      x * stats::pgamma(x, k - 1, lower.tail = FALSE) - x * p
+    # gp = g' could be simplified.
+    gp <- (k - 1) * stats::dgamma(x, k) +
+      stats::pgamma(x, k - 1, lower.tail = FALSE) -
+      x * dgamma(x, k - 1) - p
+    diff <- g / gp
+    if (x - diff <= 0) diff <- x / 2
+    x <- x - diff
+    while (abs(diff) > bd) {
+      diff <- diff / 2
+      x <- x + diff
+    }
+    iter <- iter + 1
+    #cat(iter,diff,x,"\n")
+  }
+  x
+}
+
+
+psi_new_inv <- function(p, k, mxiter = 20, eps = 1.e-12, bd = 5){
+  lengths <- c(p = length(p), k = length(k))
+  l <- max(lengths)
+  if (lengths[["p"]] == 1) p <- rep(p, l)
+  if (lengths[["k"]] == 1) k <- rep(k, l)
+  x <- numeric()
+  for (i in 1:l) {
+    x[i] <- psi_new_inv_algo(
+      p[i], k = k[i], mxiter = mxiter, eps = eps, bd = bd
+    )
+  }
+  x
+}
+
 H_new <- function(x, eta, k) {
   exp(-x) * psi_new(eta * x, k)
 }
