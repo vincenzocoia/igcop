ed17467f6cff8a3c9704c99e41bcafe45bfa9fe2
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-05-21 11:20:49 -0700

    rm igamma() examples

--- R/igamma.R
+++ R/igamma.R
@@ -11,14 +11,6 @@
 #' @details
 #' The (Upper) Incomplete Gamma Function is defined as
 #' \deqn{\Gamma(s, x) = \int_x^{\infty} t^{s - 1} e^{-t} dt}.
-#' @examples
-#' igamma(1:5, 1:5)
-#' igamma(0.4, 1:5)
-#' igamma(1:5, 0.4)
-#'
-#' # Should be identical:
-#' igamma(1:5, 0)
-#' gamma(1:5)
 igamma <- function(s, x) {
   gamma(s) * stats::pgamma(x, shape = s, lower.tail = FALSE)
 }
