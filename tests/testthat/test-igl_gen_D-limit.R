bafa577a10f67214d1947dc491b50e3c65b831f8
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-09-24 12:56:00 -0700

    check interp_gen_D1 at 0 for code coverage

--- tests/testthat/test-igl_gen_D-limit.R
+++
@@ -1,7 +0,0 @@
-test_that("Limit of igl_gen_D at 0 (from the right) works.", {
-  avec <- c(0.2, 0.5, 1, 1, 1, 1.8, 100)
-  at_zero <- igl_gen_D(0, alpha = avec)
-  near_zero <- igl_gen_D(1e-15, alpha = avec)
-  near_zero <- vctrs::vec_assign(near_zero, near_zero < -1e6, -Inf)
-  expect_equal(at_zero, near_zero, tolerance = 1e-4)
-})
--- tests/testthat/test-limit_points.R
+++ tests/testthat/test-limit_points.R
@@ -1,8 +1,25 @@
-context("Check limit points of copulas")
+context("Check limit points")
 
 test_that("Check boundaries of cdf", {
   u <- c(0, 0.7, 1, 0.7)
   v <- c(0.7, 0, 0.7, 1)
   expect_equal(pig(u, v, theta = 1, alpha = 1), c(0, 0, 0.7, 0.7))
   expect_equal(pigl(u, v, alpha = 1), c(0, 0, 0.7, 0.7))
+})
+
+test_that("Limit of igl_gen_D at 0 (from the right) works.", {
+  avec <- c(0.2, 0.5, 1, 1, 1, 1.8, 100)
+  at_zero <- igl_gen_D(0, alpha = avec)
+  near_zero <- igl_gen_D(1e-15, alpha = avec)
+  near_zero <- vctrs::vec_assign(near_zero, near_zero < -1e6, -Inf)
+  expect_equal(at_zero, near_zero, tolerance = 1e-4)
+})
+
+test_that("Limit of interp_gen_D1 at 0 (from the right) works, via pcondig12", {
+  cpar <- .cpar[-(4:5)] # alpha < 0 results in Inf/Inf.
+  for (cpar_ in cpar) {
+    at_zero <- pcondig12(.u, 0, theta = cpar_[1], alpha = cpar_[2])
+    near_zero <- pcondig12(.u, 1e-15, theta = cpar_[1], alpha = cpar_[2])
+    expect_equal(at_zero, near_zero, tolerance = 1e-4)
+  }
 })
